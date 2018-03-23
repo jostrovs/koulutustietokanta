7768fd1045fc3b2408988d2365ab394340dca986
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2018-03-23 15:51:15 +0200

    Koulutusten sorttaus

--- app/components/koulutukset.js
+++ app/components/koulutukset.js
@@ -7,9 +7,9 @@ Vue.component('vue-koulutukset',{
     <div id="vue-koulutukset">
         <div>Osallistujia yhteensä: {{total}}</div>
         <div style="display: flex">
-            <div style="flex: 0.3; font-weight: bold">Pvm</div>
-            <div style="flex: 1; font-weight: bold">Tilaisuus</div>
-            <div style="flex: 1; font-weight: bold">Kouluttaja</div>
+            <div style="flex: 0.3; font-weight: bold" @click="sortPvm">Pvm {{sort.pvm}}</div>
+            <div style="flex: 1; font-weight: bold" @click="sortTilaisuus">Tilaisuus {{sort.tilaisuus}}</div>
+            <div style="flex: 1; font-weight: bold" @click="sortKouluttaja">Kouluttaja  {{sort.kouluttaja}}</div>
             <div style="flex: 0.4; font-weight: bold">Osallistujia</div>
             <div style="flex: 1; font-weight: bold">Tools</div>
         </div>
@@ -20,7 +20,7 @@ Vue.component('vue-koulutukset',{
             <div style="flex: 0.4;"></div>
             <div style="flex: 1;"></div>
         </div>
-        <div v-for="koulutus in filtered" style="display: flex" :key="koulutus.id">
+        <div v-for="koulutus in sorted" style="display: flex" :key="koulutus.id">
             <div style="flex: 0.3">{{koulutus.pvm}}</div>
             <div style="flex: 1">{{koulutus.tilaisuus}}</div>
             <div style="flex: 1">{{koulutus.kouluttaja}}</div>
@@ -41,14 +41,40 @@ Vue.component('vue-koulutukset',{
             koulutukset: this.data.koulutukset,
             read_only: this.data.read_only,
             total: 0,
+            sort: {
+                pvm: 0,
+                tilaisuus: 0,
+                kouluttaja: 0,
+            }
         }
     },
     methods: {
         edit(koulutus){
             Bus.emit(Bus.EDIT_KOULUTUS, koulutus);
-        }
-    },
-    computed: {
+        },
+
+        sortPvm(){
+            if(this.sort.pvm < 0) this.sort.pvm = 1;
+            else if(this.sort.pvm > 0) this.sort.pvm = 0;
+            else this.sort.pvm = -1;
+        },
+        sortTilaisuus(){
+            if(this.sort.tilaisuus < 0) this.sort.tilaisuus = 1;
+            else if(this.sort.tilaisuus > 0) this.sort.tilaisuus = 0;
+            else this.sort.tilaisuus = -1;
+        },
+        sortKouluttaja(){
+            if(this.sort.kouluttaja < 0) this.sort.kouluttaja = 1;
+            else if(this.sort.kouluttaja > 0) this.sort.kouluttaja = 0;
+            else this.sort.kouluttaja = -1;
+        },
+
+        sortFunc(a,b){
+            if(a < b) return -1;
+            if(a > b) return 1;
+            return 0;
+        },
+
         filtered(){
             let self=this;
 
@@ -65,5 +91,25 @@ Vue.component('vue-koulutukset',{
             
             return ret;
         },
+
+    },
+    computed: {
+        sorted(){
+            let self=this;
+
+            let ret = this.filtered();
+
+            if(this.sort.pvm != 0){
+                return ret.sort((a,b)=>self.sortFunc(a.pvm, b.pvm));
+            }
+            if(this.sort.tilaisuus != 0){
+                return ret.sort((a,b)=>self.sortFunc(a.tilaisuus, b.tilaisuus));
+            }
+            if(this.sort.kouluttaja != 0){
+                return ret.sort((a,b)=>self.sortFunc(a.kouluttaja, b.kouluttaja));
+            }
+
+            return ret;
+        }
     }
 });
